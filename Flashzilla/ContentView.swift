10752eab9d834d75e7c2a59ed3132210b950235a
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-27 17:58:43 -0300

    Building a stack of cards

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -13,9 +13,46 @@ import SwiftUI
 // To force Landscape, go to blue icon Flashzilla > Targets > Flashzilla > Info > Delete Portrait options on Supported interface orientations (iPhone).
 // Switch to Landscape Mode on the iOS Simulator by pressing Command + <- Arrow
 
+// Our next step is to build a stack of those cards to represent the subject the user is learning.
+// This stack will change as the app is being used: it will remove cards over time. So, it will be represented with @State. In the beginning we will use a stack of 10 example cards by calling Swift's Array init(repeating:count:), which takes one value and repeats it n times.
+
+// For now, we will do as follows:
+// 1. Our stack of cards will be placed inside a single ZStack, so we can place them partly overlapped with a 3D effect.
+// 2. Around the ZStack, there will be a VStack. Later on, we will use it to add a timer above our cards.
+// 3. Around the VStack, there will be another ZStack, so we can place the cards and timer on a background.
+// The complex part is how to position the cards inside the ZStack so they have a slight overlapping. The best way to write SwiftUI code is to give messy calculations to methods or modifiers.
+// In this case, we will create a stacked() method that takes a position in an Array and its length, and offsets a View by some amount based on this parameters. This way, each card will be a little further down the screen than the one before.
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        // Calculating our offset: how far through we are in the deck
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10) // self is the current View, which will be 10 points down as the position increases
+    }
+}
+
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            // The background is found at: https://github.com/twostraws/HackingWithSwift/tree/main/SwiftUI/project17-files
+            // Both pictures were added to the assets
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea() // So it goes edge to edge
+            
+            VStack {
+                ZStack {
+                    // We get a really thick shadow because many cards are overlapping: as the cards depth increases, the shadow increases
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+        
+        
     }
 }
 
